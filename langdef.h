cb7897b55e8cce51f4bd3dc7162da3bc1fdb0a3e
C.EXE <c-exe@users.noreply.github.com>
2016-09-02 23:44:51 +0100

    Started program menus + languages
    Started drafting the program's menus and added compilation options to
    ease translation to different languages.

---
+++ langdef.h
@@ -0,0 +1,10 @@
+/* Languages Definition */
+
+#ifndef __LANGDEF_H__
+
+#define __LANGDEF_H__
+
+#define LANG_ENGLISH 1
+#define LANG_FRENCH 2
+
+#endif
---
+++ resource.h
@@ -0,0 +1,13 @@
+/* Resources Header */
+
+#ifndef __RESOURCE_H__
+
+#define __RESOURCE_H__ 1
+
+
+#define IDR_MAINMENU 101
+
+#define ID_FILE_EXIT 9001
+
+
+#endif
