399ad2aa80231e88da905ff3fba21181a326c5ac
C.EXE <c-exe@users.noreply.github.com>
2016-09-02 23:48:29 +0100

    Added language choice section.
    Added a point where compilers can choose their language.

--- langdef.h
+++ langdef.h
@@ -5,6 +5,13 @@
 #define __LANGDEF_H__
 
 #define LANG_ENGLISH 1
-#define LANG_FRENCH 2
+#define LANG_FRANCAIS 2
+#define LANG_CYMRAIG 3
+
+
+/* ------------- Define chosen languages below this point ------------- */
+
+#define MENULANG LANG_ENGLISH
+#define DIALOGUELANG LANG_ENGLISH
 
 #endif
