e464af037f7b93b6a22b97cd1162762b890e8898
C.EXE <c-exe@users.noreply.github.com>
2016-09-03 23:35:53 +0100

    Continued menus and wrote info resources
    Program info resource file and settings part of menu done.

--- resource.h
+++ resource.h
@@ -4,6 +4,42 @@
 
 #define __RESOURCE_H__ 1
 
+#include "langdef.h"
+#ifndef INFOLANG
+#define INFOLANG LANG_ENGLISH
+#endif
+
+#ifndef VS_VERSION_INFO
+#define VS_VERSION_INFO 1
+#endif
+
+#define VER_FILEVERSION 1,0,1,0
+#define VER_FILEVERSION_STR "1.00.01\0"
+#define VER_PRODUCTVERSION 4,0,0,1
+#define VER_PRODUCTVERSION_STR "4.00.00 ALPHA\0"
+#if INFOLANG == LANG_ENGLISH
+#define VER_COMPANYNAME_STR "The Open Trebuchet Project\0"
+#define VER_PRODUCTNAME_STR "Open Trebuchet\0"
+#define VER_FILEDESCRIPTION_STR "Open Trebuchet - Open-source Windows projection software.\0"
+#define VER_INTERNALNAME_STR "ot4\0"
+#define VER_ORIGINALFILENAME_STR "ot4\0"
+#define VER_LEGALCOPYRIGHT_STR "2016 The Open Trebuchet Project\0"
+#elif INFOLANG == LANG_FRANCAIS
+#define VER_COMPANYNAME_STR "Le projet Open Trebuchet\0"
+#define VER_PRODUCTNAME_STR "Open Trebuchet\0"
+#define VER_FILEDESCRIPTION_STR "Open Trebuchet - Open-source du logiciel de projection de Windows.\0"
+#define VER_INTERNALNAME_STR "ot4\0"
+#define VER_ORIGINALFILENAME_STR "ot4\0"
+#define VER_LEGALCOPYRIGHT_STR "2016 Le projet Open Trebuchet\0"
+#elif INFOLANG == LANG_CYMRAIG
+#define VER_COMPANYNAME_STR "Mae'r Prosiect Open Trebuchet\0"
+#define VER_PRODUCTNAME_STR "Open Trebuchet\0"
+#define VER_FILEDESCRIPTION_STR "Open Trebuchet - Meddalwedd taflunio ffynhonnell agored ar gyfer Windows.\0"
+#define VER_INTERNALNAME_STR "ot4\0"
+#define VER_ORIGINALFILENAME_STR "ot4\0"
+#define VER_LEGALCOPYRIGHT_STR "2016 Mae'r Prosiect Open Trebuchet\0"
+#endif
+
 
 #define IDR_MAINMENU 101
 
@@ -12,6 +48,7 @@
 #define ID_SHOW_PROJECT 9003
 #define ID_SHOW_BLACK 9004
 #define ID_SHOW_CLEAR 9005
+#define ID_SHOW_SETTINGS 9006
 
 
 #endif
